c2fe9fd9a32b1d9982838dc8e1a81b69cf59666e
Andrey Gorokhov <36040108+andreyalexeich@users.noreply.github.com>
2018-02-15 19:05:02 +0300

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm install gulp@next browser-sync gulp-autoprefixer gulp-uglify gulp-sass gulp-clean-css gulp-concat gulp-rename gulp-svgo gulp-imagemin imagemin-pngquant imagemin-jpeg-recompress  gulp-favicons gulp-plumber ngrok gulp-newer gulp-debug gulp-watch --save-dev --save-exact
+npm install gulp@next browser-sync gulp-autoprefixer gulp-uglify gulp-sass gulp-clean-css gulp-concat gulp-rename gulp-svgo gulp-imagemin imagemin-pngquant imagemin-jpeg-recompress  gulp-favicons gulp-plumber ngrok gulp-newer gulp-debug gulp-rimraf gulp-watch --save-dev --save-exact

b599fd5e3664c3d60b74b59d3889da49122cbaa6
Andrey Gorokhov <36040108+andreyalexeich@users.noreply.github.com>
2018-08-31 22:30:08 +0300

    Update watch.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ module.exports = function() {
     $.gulp.task("watch", function() {
         return new Promise((res, rej) => {
             $.gp.watch("./src/**/*.html", $.gulp.series("html"));
-            $.gp.watch("./src/styles/**/*.sass", $.gulp.series("styles"));
+            $.gp.watch("./src/styles/**/*.scss", $.gulp.series("styles"));
             $.gp.watch(["./src/img/**/*.{jpg,jpeg,png,gif}", "!./src/img/icons/svg/*.svg", "!./src/img/favicons/*.{jpg,jpeg,png,gif}"], $.gulp.series("images"));
             $.gp.watch("./src/img/icons/svg/*.svg", $.gulp.series("svg"));
             $.gp.watch("./src/js/**/*.js", $.gulp.series("scripts"));

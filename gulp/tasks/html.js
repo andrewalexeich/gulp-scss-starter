43a4af8e29dc69d9a6f691c6c32488d82275a96f
Andrey Gorokhov <ag.alexeich@gmail.com>
2019-01-13 21:44:02 +0300

    Minor fixes and updates

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -2,7 +2,6 @@ module.exports = function() {
     $.gulp.task("html", function() {
         return $.gulp.src(["./src/views/**/index.html", "!./src/views/blocks/*.html"])
             .pipe($.rigger())
-            .pipe($.replace("../dest/", "../"))
             .pipe($.gulp.dest("./dest/"))
             .pipe($.debug({"title": "html"}))
             .on("end", $.browsersync.reload);

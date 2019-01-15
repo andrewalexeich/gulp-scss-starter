1565669a0a070673d9e7ed3632f0b06f75b063a9
Andrey Gorokhov <ag.alexeich@gmail.com>
2019-01-16 00:42:03 +0300

    Tasks for dev and prod mods

--- gulp/tasks/clean.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-    $.gulp.task("clean", function() {
-        return $.gulp.src("./dest/*", {read: false})
-            .pipe($.clean())
-            .pipe($.debug({"title": "clean"}));
-    });
-};
